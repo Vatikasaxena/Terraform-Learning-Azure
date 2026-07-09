c6006e4a9df7db8d05a32da84c07baa9910c9e55
Vatikasaxena <114807561+Vatikasaxena@users.noreply.github.com>
2026-07-09 17:45:05 +0000

    Create variables.tf

---
+++ VM/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
